420df62062b647c9ca46340563cfece3df15d88f
NielsHam1 <niels.ham1@student.graafschapcollege.nl>
2026-04-24 10:13:28 +0200

    Create voorraad.php

---
+++ public/voorraad.php
@@ -0,0 +1,16 @@
+<h1>Alle Voorraad</h1>
+
+<p>Klik <a href="index.php">hier</a> om terug te gaan.</p>
+<p>Klik <a href="">hier</a> om voorraad aan te passen.</p>
+
+<table>
+    <thead>
+        <tr>
+            <td>Wat</td>
+            <td>Hoeveel</td>
+        </tr>
+    </thead>
+<?php
+// hier komt de code voor het maken van de tabel
+?>
+</table>
