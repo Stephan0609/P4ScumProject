2ca1517530a793ae2259be73090a9c991aa3cf5a
Quinten250 <quinten.lukassen@student.graafschapcollege.nl>
2026-04-24 20:00:24 +0200

    voorraad aanpassen gemaakt

--- public/voorraad.php
+++ public/voorraad.php
@@ -1,7 +1,7 @@
 <h1>Alle Voorraad</h1>
 
 <p>Klik <a href="index.php">hier</a> om terug te gaan.</p>
-<p>Klik <a href="">hier</a> om voorraad aan te passen.</p>
+<p>Klik <a href="newStock.php">hier</a> om voorraad aan te passen.</p>
 
 <table>
     <thead>
