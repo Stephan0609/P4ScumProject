921743760e988afa916d05d6f8009228e4b224ef
Stephan0609 <stephan.roordink@student.graafschapcollege.nl>
2026-05-13 15:35:31 +0200

    Factuur Verstuur en Betaald checkbox toegevoegt

--- public/klantdetail.php
+++ public/klantdetail.php
@@ -31,22 +31,44 @@ echo "<p>Email: $email<br>Telefoon: $phone<br>Adres: $address</p>";
 echo("<a href='newJob.php?id=$id'>Nieuwe klus</a>");
 
 $jobs = new Jobs;
-$tasks = $jobs->GetAllJobsWithCustomerID($id);
-echo "<table><thead><tr>
-<td>Titel</td><td>Beschrijving</td><td>Locatie</td>
-</tr></thead><tbody>";
-foreach ($tasks as $t) {
-    echo "<tr>";
-    $title = $t['title'];
-    echo "<td>$title</td>";
-    $desc = $t['description'];
-    echo "<td>$desc</td>";
-    $loc = $t['location'];
-    echo "<td>$loc</td>";
-    echo "</tr>";
-}
-echo "</tbody></table>";
 
+if (isset($_POST["updateJob"])) {
+    $send = 0;
+    $paid = 0;
+    if(isset($_POST["send"])) {
+        $send = 1;
+    } 
+    if(isset($_POST["paid"])) {
+        $paid = 1;
+    }
+
+    $jobs->updateInvoiceSendAndPaid($send, $paid, $_POST["id"]);
+}
+
+$tasks = $jobs->GetAllJobsWithCustomerID($id);
+?>
+
+<table border='1'><thead>
+    <tr>
+        <td>Titel</td><td>Beschrijving</td><td>Locatie</td><td>Factuur Verstuurd</td><td>Factuur Betaald</td><td>Update</td>
+    </tr>
+    <?php foreach ($tasks as $t):?>
+        <form action="" method="post">
+        <input type="hidden" name="id" value="<?= $t['id'] ?>">
+        <tr>
+            <td><?= $t['title'] ?></td>
+            <td><?= $t['description'] ?></td>
+            <td><?= $t['location'] ?></td>
+            <td><input type="checkbox" name="send" <?php if ($t['invoiceSent'] == 1) echo "checked" ?>></td>
+            <td><input type="checkbox" name="paid" <?php if ($t['paid'] == 1) echo "checked" ?>></td>
+            <td><input type="submit" name="updateJob" value="update"></td>
+        </tr>
+        </form>
+    <?php endforeach?>
+</thead>
+<tbody>
+
+<?php
 if(isset($_POST['veranderAdres']))
     {
         if(isset($_POST['nieuwAdres']))
