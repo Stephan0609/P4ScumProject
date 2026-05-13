e2ba0b3dc22dfd340c3b1e8eb4000f9f1de2eae9
NielsHam1 <niels.ham1@student.graafschapcollege.nl>
2026-05-13 15:21:58 +0200

    melding met weinig voorraad

--- public/voorraad.php
+++ public/voorraad.php
@@ -5,6 +5,12 @@ if (!isset($_SESSION["email"])) {
 }
 ?>
 
+<style>
+    .low {
+        background-color: orange;
+    }
+</style>
+
 <h1>Alle Voorraad</h1>
 
 <p>Klik <a href="index.php">hier</a> om terug te gaan.</p>
@@ -24,13 +30,31 @@ if (!isset($_SESSION["email"])) {
         // hier komt de code voor het maken van de tabel
         $result = $stock->GetAllStock();
         foreach ($result as $r) {
-            echo "<tr>";
+            $quantity = $r["quantity"];
+            echo "<tr";
+            if ($quantity <= 3) {
+                echo " class='low'";
+            }
+            echo ">";
             $name = $r["name"];
             echo "<td>$name</td>";
-            $quantity = $r["quantity"];
             echo "<td>$quantity</td>";
             echo "</tr>";
         }
         ?>
     </tbody>
 </table>
+
+<script>
+    const low = document.getElementsByClassName("low");
+    if (low.length > 0) {
+        var alertText = "Je hebt weinig voorraad van de volgende: "
+        Array.from(low).forEach(element => {
+            const material = element.children[0].textContent
+            alertText += material + ", "
+        });
+        alert(alertText);
+    }
+
+//  
+</script>
