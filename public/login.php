9b0217f049e10484ca73f1e1fe313b3206cd16d4
Stephan0609 <stephan.roordink@student.graafschapcollege.nl>
2026-05-27 14:38:32 +0200

    login.php css

--- public/login.php
+++ public/login.php
@@ -11,27 +11,35 @@ session_start();
 if (isset($_SESSION["email"])) {
     header("Location: index.php");
 }
-
-if (isset($_POST["send"]) && $_POST["email"] != "") {
-    $user = $Authenticate->UserLogin($_POST["email"], $_POST["password"]);
-
-    if (!$user) {
-        echo "Onjuiste inloggegevens";
-    } else {
-        $_SESSION["email"] = $user;
-        header("Location: index.php");
-    }
-} else {
-    echo "Vul alles in";
-}
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<section class="container">
+    <form action="" method="post" class="login">
+        <?php
+        if (isset($_POST["send"]) && $_POST["email"] != "") {
+            $user = $Authenticate->UserLogin($_POST["email"], $_POST["password"]);
 
-<form action="" method="post">
-    <label for="email">Email:</label><br>
-    <input type="email" name="email" id=""><br>
-    <label for="password">Wachtwoord</label><br>
-    <input type="password" name="password" id=""><br>
-    <br>
-    <input type="submit" name="send" value="login">
-</form>
+            if (!$user) {
+                echo "Onjuiste inloggegevens";
+            } else {
+                $_SESSION["email"] = $user;
+                header("Location: index.php");
+            }
+        } else {
+            echo "Vul alles in";
+        }
+        ?>
+        <input type="email" name="email" id="" placeholder="Email"><br>
+        <input type="password" name="password" id="" placeholder="Wachtwoord"><br>
+        <br>
+        <input type="submit" name="send" value="login">
+    </form>
+</section>
